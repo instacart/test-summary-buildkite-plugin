5e35396cfa1dc1a1c09a50715c5772c0acef469a
Kevin Green <kevin.green@instacart.com>
2022-08-31 18:23:50 -0400

    Remove unneeded changes

--- lib/test_summary_buildkite_plugin/haml_render.rb
+++ lib/test_summary_buildkite_plugin/haml_render.rb
@@ -3,15 +3,10 @@
 module TestSummaryBuildkitePlugin
   class HamlRender
     def self.render(name, params, folder: nil)
-      filename = [
-          File.join(ROOT_DIR, "templates", folder, "#{name}.html.haml"), 
-          File.join(ROOT_DIR, "templates", "#{name}.html.haml")
-        ].find { |f| File.exist?(f) }
+      filename = %W[#{ROOT_DIR}/templates/#{folder}/#{name}.html.haml #{ROOT_DIR}/templates/#{name}.html.haml].find { |f| File.exist?(f) }
       if filename
         engine = Haml::Engine.new(File.read(filename), escape_html: true)
         engine.render(Object.new, params)
-      else
-        puts "Cannot find template with name #{name}, #{ROOT_DIR}/templates/#{folder}/#{name}.html.haml"
       end
     end
   end
