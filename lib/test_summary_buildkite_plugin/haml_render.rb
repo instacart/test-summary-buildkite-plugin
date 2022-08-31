d488feea7c5b0c168a643ad79fb97acb6614e25f
Kevin Green <kevin.green@instacart.com>
2022-08-31 18:20:09 -0400

    Singular

--- lib/test_summary_buildkite_plugin/haml_render.rb
+++ lib/test_summary_buildkite_plugin/haml_render.rb
@@ -4,8 +4,8 @@ module TestSummaryBuildkitePlugin
   class HamlRender
     def self.render(name, params, folder: nil)
       filename = [
-          Files.join(ROOT_DIR, "templates", folder, "#{name}.html.haml"), 
-          Files.join(ROOT_DIR, "templates", "#{name}.html.haml")
+          File.join(ROOT_DIR, "templates", folder, "#{name}.html.haml"), 
+          File.join(ROOT_DIR, "templates", "#{name}.html.haml")
         ].find { |f| File.exist?(f) }
       if filename
         engine = Haml::Engine.new(File.read(filename), escape_html: true)
